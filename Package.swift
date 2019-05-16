c8cdaf0d96fd6ca3f8445ee423c51ba9d8fff3ba
Sendy Halim <sendyhalim93@gmail.com>
2019-05-17 00:22:36 +0700

    Add SPM support

---
+++ Package.swift
@@ -0,0 +1,20 @@
+
+import PackageDescription
+
+let package = Package(
+  name: "SwiftTheme",
+  products: [
+    .library(name: "SwiftTheme", targets: ["SwiftTheme"])
+  ],
+  dependencies: [
+    .package(url: "https://github.com/ZipArchive/ZipArchive", from: "2.0.8"),
+    .package(url: "https://github.com/jdg/MBProgressHUD", .from: "1.0.0")
+  ],
+  targets: [
+    .target(
+      name: "SwiftTheme",
+      dependencies: []
+    )
+  ]
+)
+
